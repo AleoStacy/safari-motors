0d57bf2d7fb9e1fe98654193f58be86dc295c025
Stacy Aleyo <aleyostacy@gmail.com>
2025-03-13 11:49:03 +0300

    no changes made

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,11 @@
-import App from "./App"
+import App from "./App";
 import Navbar from "./components/Navbar";
 import LandingPage from "./Pages/LandingPage";
 import Booking from "./Pages/Booking";
 import Contacts from "./Pages/Contacts";
 import Gallery from "./Pages/Gallery";
 import Home from "./Pages/Home";
-import Reviews from "./Pages/Reviews";
+import Reviews from "./Pages/ReviewSection";
 import Safari from "./Pages/Safari";
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
@@ -13,28 +13,24 @@ import ForgotPassword from "./Pages/ForgotPassword";
 import ResetPassword from "./Pages/ResetPassword";
 
 const routes = [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {path:"/", element:<LandingPage/>}, 
-        {path:"/home", element:<Home/>},
-        {path:"/booking", element:<Booking/>},
-        {path:"/contacts", element:<Contacts/>},
-        {path:"/gallery", element:<Gallery/>},
-        {path:"/reviews", element:<Reviews/>},
-        {path:"/safari", element:<Safari/>},
-        {path: "/login", element: <Login/>},
-        {path: "/signup", element: <SignUp/>},
-        {path: "/contacts", element: <Contacts/>},
-        {path: "/forgot-password", element: <ForgotPassword/>},
-        {path: "/reset-password", element: <ResetPassword/>},
-        
-
-      ],
-    
-    },
-
-]
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+      { path: "/home", element: <Home /> },
+      { path: "/booking", element: <Booking /> },
+      { path: "/contacts", element: <Contacts /> },
+      { path: "/gallery", element: <Gallery /> },
+      { path: "/reviews", element: <Reviews /> },
+      { path: "/safari", element: <Safari /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <SignUp /> },
+      { path: "/contacts", element: <Contacts /> },
+      { path: "/forgot-password", element: <ForgotPassword /> },
+      { path: "/reset-password", element: <ResetPassword /> },
+    ],
+  },
+];
 
 export default routes;
